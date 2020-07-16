f5238dbe0ad9c464f8a5d3f283480cf6daf9d159
Nathan Hall <Nashing1@gmail.com>
2020-07-15 22:32:36 -0500

    minor changes to the front end
    added data attributes to the list display buttons and also added the wishlist generator. still need to add a notes section to the wishlist

--- assets/js/wishlist.js
+++ assets/js/wishlist.js
@@ -1,3 +1,32 @@
 $(document).ready(() =>{
-    
+    $.ajax({
+        url: "http://localhost:3000/animals/wishlist/2", 
+        method: 'GET',
+        success: function(response){
+            console.log(response);
+            let x = 0;
+            while ( x <= response.length){
+                $("tbody").append(`
+                <tr>
+                    <td class="listImg">
+                        <img src="${response[x].ImageURL}" alt="pet image" height="150">
+                    </td>
+                    <td class="name">
+                        <p>${response[x].PetName}</p>
+                    </td>
+                    <td class="location">
+                        <p>Chicago</p>
+                    </td>
+                    <td class="details">
+                        <button type="button" data-id="${response[x].PetID}" data-user="${response[x].UserID}" class="btn btn-secondary btn-lg viewPet">View Details</button>
+                    </td>
+                    <td class="remove">
+                        <button type="button" data-id="${response[x].PetID}" data-user="${response[x].UserID}" class="btn btn-secondary btn-lg removePet">Remove from list?</button>
+                    </td>
+                </tr>
+                `);
+                x++;
+            }
+        }
+    });
 });
